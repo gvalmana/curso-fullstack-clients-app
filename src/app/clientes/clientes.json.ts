aa0b1e2b458dedc8754a80408e8c9998480c48dd
gvalmana <gustavo.shotokai@gmail.com>
2024-01-08 22:48:30 -0500

    creado servicio

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,46 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {
+    id: 1,
+    nombre: 'Gustavo',
+    apellido: 'Valmaña',
+    email: 'kYrXh@example.com',
+    createAt: '2022-10-27',
+  },
+  {
+    id: 2,
+    nombre: 'Andrés',
+    apellido: 'Valmaña',
+    email: 'kYrXh@example.com',
+    createAt: '2022-10-27',
+  },
+  {
+    id: 3,
+    nombre: 'Gustavo',
+    apellido: 'Valmaña',
+    email: 'kYrXh@example.com',
+    createAt: '2022-10-27',
+  },
+  {
+    id: 4,
+    nombre: 'Gustavo',
+    apellido: 'Valmaña',
+    email: 'kYrXh@example.com',
+    createAt: '2022-10-27',
+  },
+  {
+    id: 5,
+    nombre: 'Gustavo',
+    apellido: 'Valmaña',
+    email: 'kYrXh@example.com',
+    createAt: '2022-10-27',
+  },
+  {
+    id: 6,
+    nombre: 'Gustavo',
+    apellido: 'Valmaña',
+    email: 'kYrXh@example.com',
+    createAt: '2022-10-27',
+  },
+];
